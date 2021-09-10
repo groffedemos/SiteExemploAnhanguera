d666ddaa42f35dd4cee567265d0ed539c64d7c42
Renato Groffe <renato.groffe@yahoo.com.br>
2021-09-10 19:40:22 -0300

    Update Program.cs

--- SiteCanaisTecnologia/Program.cs
+++ SiteCanaisTecnologia/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//
 namespace SiteCanaisTecnologia
 {
     public class Program
